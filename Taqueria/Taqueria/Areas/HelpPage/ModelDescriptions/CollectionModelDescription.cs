e5d903ce83816f6e11644cab977ca5eea7bcc613
Yesid Alcaraz <catadorverde@gmail.com>
2020-12-26 22:22:39 +0100

    APITAC
    Service

---
+++ Taqueria/Taqueria/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Taqueria.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Taqueria/Taqueria/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Taqueria.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
