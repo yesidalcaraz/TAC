392c1ab8f0bfde34b68a6c3144d381088feb0e31
Yesid Alcaraz <catadorverde@gmail.com>
2020-12-26 22:24:54 +0100

    TacAPI
    TacAPI

---
+++ Taqueria/Taqueria/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Taqueria.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Models.tacos> Get()
+        {
+            IEnumerable<Models.tacos> lst;
+            using (Models.TaqueroEntities db = new Models.TaqueroEntities())
+            {
+                lst = db.tacos.ToList();
+            }
+            return lst;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
