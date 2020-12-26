0b798bf7ab861d49572aea0a7dea05c5ef7c979e
Yesid Alcaraz <catadorverde@gmail.com>
2020-12-26 23:12:06 +0100

    MySoap
    MySoap

---
+++ MySoap/Cliente/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MiServicio.Service1Client oCliente = new MiServicio.Service1Client();
+            string res= oCliente.GetData(10,5);
+            MessageBox.Show(res);
+
+            MiServicio.CompositeType oData = new MiServicio.CompositeType();
+            oData.BoolValue = true;
+            var res2= oCliente.GetDataUsingDataContract(oData);
+            MessageBox.Show(res2.StringValue);
+
+
+        }
+    }
+}
